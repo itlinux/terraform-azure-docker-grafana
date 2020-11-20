362e18bd26e29989ada1d978c8f55bff77f422f2
Remo Mattei <remo@MacBook-Pro.local>
2020-11-20 12:34:56 -0800

    adding defaults.ini and update grafana.tf

--- grafana.tf
+++ grafana.tf
@@ -12,9 +12,9 @@ resource "null_resource" "grafana" {
     inline = [
       "sudo mkdir -p /grafana",
       "sudo mkdir -p /prometheus",
+      "sudo chown -R $USER:$USER /grafana",
       "curl -o /grafana/grafana.ini https://raw.githubusercontent.com/grafana/grafana/master/conf/defaults.ini",
       "curl -o /tmp/prometheus.yml https://raw.githubusercontent.com/itlinux/terraform-azure-docker-grafana/master/prometheus.yml",
-      "sudo chown -R $USER:$USER /grafana",
       "docker run -d -p 3000:3000 -uroot --name grafana -v /grafana/:/etc/grafana/ -v /grafana:/var/lib/grafana grafana/grafana",
       "docker run -d -p 9090:9090 --name prometheus -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus"
     ]
