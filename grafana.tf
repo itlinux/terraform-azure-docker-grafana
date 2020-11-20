a1115f65eadbc6be43a1fda17d7037b8d3935c02
Remo Mattei <rm@rm.ht>
2020-11-20 11:46:48 -0800

    update to point to my repo for prometheus

--- grafana.tf
+++ grafana.tf
@@ -13,7 +13,7 @@ resource "null_resource" "grafana" {
       "sudo mkdir -p /grafana",
       "sudo mkdir -p /prometheus",
       "curl -o /grafana/grafana.ini https://raw.githubusercontent.com/grafana/grafana/master/conf/defaults.ini",
-      "curl -o /tmp/prometheus.yml https://gist.githubusercontent.com/noemi-dresden/38a63199cedaaae83a0f9f3a8ff30bd6/raw/81b22b83c67e69c971fd3d0d31d61969cd5b7fd9/prometheus.yml",
+      "curl -o /tmp/prometheus.yml https://raw.githubusercontent.com/itlinux/terraform-azure-docker-grafana/master/prometheus.yml",
       "sudo chown -R $USER:$USER /grafana",
       "docker run -d -p 3000:3000 -uroot --name grafana -v /grafana/:/etc/grafana/ -v /grafana:/var/lib/grafana grafana/grafana",
       "docker run -d -p 9090:9090 --name prometheus -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus"
